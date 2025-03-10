9b62936157506e36e668d232d72fbf166963c743
xlokox <knafel10@gmail.com>
2025-03-10 17:46:15 +0200

    adding banners for the Header

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,63 +1,56 @@
-import React, { useEffect } from 'react';
-import Carousel from 'react-multi-carousel';
-import { Link } from 'react-router-dom';
-import 'react-multi-carousel/lib/styles.css'
-import { useDispatch, useSelector } from 'react-redux';
-import { get_banners } from '../store/reducers/homeReducer';
-
-const Banner = () => {
-
-    const dispatch = useDispatch()
-    const {banners} = useSelector(state => state.home)
+import React from 'react';
+ import Carousel from 'react-multi-carousel';
+ import { Link } from 'react-router-dom';
+ import 'react-multi-carousel/lib/styles.css'
  
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 1
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 1
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 1
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        },
-    }
-
-    useEffect(() => {
-        dispatch(get_banners())
-    },[])
-
-    return (
-        <div className='w-full md-lg:mt-6'>
-            <div className='w-[85%] lg:w-[90%] mx-auto'>
-                <div className='w-full flex flex-wrap md-lg:gap-8'>
-                    <div className='w-full'>
-                        <div className='my-8'>
-                <Carousel
-                    autoPlay={true}
-                    infinite={true}
-                    arrows={true}
-                    showDots={true}
-                    responsive={responsive}
-                >
-                {
-                   banners.length > 0 && banners.map((b, i) => <Link key={i} to={`product/details/${b.link}`}>
-                        <img src={ b.banner} alt="" />
-                    </Link> )
-                }
-                </Carousel>        
-                        </div>
-                    </div>
-                </div> 
-            </div> 
-        </div>
-    );
-};
-
-export default Banner;
+ const Banner = () => {
+ 
+     const responsive = {
+         superLargeDesktop: {
+             breakpoint: { max: 4000, min: 3000 },
+             items: 1
+         },
+         desktop: {
+             breakpoint: { max: 3000, min: 1024 },
+             items: 1
+         },
+         tablet: {
+             breakpoint: { max: 1024, min: 464 },
+             items: 1
+         },
+         mobile: {
+             breakpoint: { max: 464, min: 0 },
+             items: 1
+         },
+     }
+ 
+ 
+ 
+     return (
+         <div className='w-full md-lg:mt-6'>
+             <div className='w-[85%] lg:w-[90%] mx-auto'>
+                 <div className='w-full flex flex-wrap md-lg:gap-8'>
+                     <div className='w-full'>
+                         <div className='my-8'>
+                 <Carousel
+                     autoPlay={true}
+                     infinite={true}
+                     arrows={true}
+                     showDots={true}
+                     responsive={responsive}
+                 >
+                 {
+                     [1,2,3,4,5,6].map((img, i) => <Link key={i} to='#'>
+                         <img src={`http://localhost:3000/images/banner/${img}.jpg`} alt="" />
+                     </Link> )
+                 }
+                 </Carousel>        
+                         </div>
+                     </div>
+                 </div> 
+             </div> 
+         </div>
+     );
+ };
+ 
+ export default Banner;
